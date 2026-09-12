375ca4cb3730010994f0f25e38df550ccd5bcf5b
Gaoshoudawang <226536392+Gaoshoudawang@users.noreply.github.com>
2026-09-12 14:51:17 +0800

    Add files via upload

---
+++ data/gacha_base.js
@@ -0,0 +1,28 @@
+window.GACHA_BASE_POOL = [
+  // ===== 金蛋（7个）=====
+  {name: '炫闪·G3A3-凤雏', quality: 'gold', image: '../images/items/gold/炫闪G3A3-凤雏.png', weight: 0.05},
+  {name: '【26年元旦扭蛋机】三国武器自选卡', quality: 'gold', image: '../images/items/gold/三国武器自选卡.png', weight: 0.45},
+  {name: '【26年元旦扭蛋机】破空枪刃黑龙魂自选卡', quality: 'gold', image: '../images/items/gold/破空枪刃黑龙魂自选卡.png', weight: 0.005},
+  {name: '【26年元旦扭蛋机】C4皮肤自选卡', quality: 'gold', image: '../images/items/gold/C4皮肤自选卡.png', weight: 0.005},
+  {name: '【26年元旦扭蛋机】副武器自选卡', quality: 'gold', image: '../images/items/gold/副武器自选卡.png', weight: 5.500},
+  {name: '三国皮肤自选卡', quality: 'gold', image: '../images/items/gold/三国皮肤自选卡.png', weight: 1.000},
+  {name: '星象投掷自选卡', quality: 'gold', image: '../images/items/gold/星象投掷自选卡.png', weight: 0.500},
+
+  // ===== 紫蛋（7个）=====
+  {name: '9A91-游骑兵', quality: 'gold', image: '../images/items/gold/9A91-游骑兵.png', weight: 3.500},
+  {name: 'M4A1-竞界荣光', quality: 'gold', image: '../images/items/gold/M4A1-竞界荣光.png', weight: 3.500},
+  {name: 'Origin12-小熊软糖', quality: 'gold', image: '../images/items/gold/Origin12-小熊软糖.png', weight: 3.500},
+  {name: 'M1887-动能冲击', quality: 'gold', image: '../images/items/gold/M1887-动能冲击.png', weight: 3.500},
+  {name: 'AK47-钢铁狂热', quality: 'gold', image: '../images/items/gold/AK47-钢铁狂热.png', weight: 4.000},
+  {name: 'AN94-超新星', quality: 'gold', image: '../images/items/gold/AN94-超新星.png', weight: 4.000},
+  {name: '巴雷特-金色蔷薇', quality: 'gold', image: '../images/items/gold/巴雷特-金色蔷薇.png', weight: 4.000},
+
+  // ===== 白蛋（7个）=====
+  {name: '沙鹰-天神', quality: 'gold', image: '../images/items/gold/沙鹰-天神.png', weight: 9.570},
+  {name: '09式狙-天神', quality: 'gold', image: '../images/items/gold/09式狙-天神.png', weight: 9.570},
+  {name: '双持斯泰尔-天使', quality: 'gold', image: '../images/items/gold/双持斯泰尔-天使.png', weight: 9.570},
+  {name: 'QSZ92-天使', quality: 'gold', image: '../images/items/gold/QSZ92-天使.png', weight: 9.570},
+  {name: '神罚战斧', quality: 'gold', image: '../images/items/gold/神罚战斧.png', weight: 9.570},
+  {name: '麒麟刺', quality: 'gold', image: '../images/items/gold/麒麟刺.png', weight: 9.570},
+  {name: '高爆手雷-雷暴', quality: 'gold', image: '../images/items/gold/高爆手雷-雷暴.png', weight: 9.070},
+];
